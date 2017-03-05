e8b5e5907dd93e7f0e43032416d5919c249b5a03
Max Chesterfield <chestm007@hotmail.com>
2017-03-05 23:54:50 +1100

    setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+rm -f /home/max/.zshrc
+rm -f /home/max/.bashrc
+rm -rf /home/max/.vim
+rm -rf /home/max/.vimrc
+rm -rf /home/max/.viminfo
+ln -s /home/max/git/configuration/vim/.vimrc /home/max/.vimrc
+ln -s /home/max/git/configuration/vim/.vim /home/max/.vim
+ln -s /home/max/git/configuration/vim/.viminfo /home/max/.viminfo
+ln -s /home/max/git/configuration/vim/.bashrc /home/max/.bashrc
+ln -s /home/max/git/configuration/vim/.zshrc /home/max/.zshrc

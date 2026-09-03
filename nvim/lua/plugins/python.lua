e4841de670841dd65b33a73fd73d3e94125960cc
chestm007 <chestm007@hotmail.com>
2026-09-03 22:31:39 +1000

    python settings

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,20 +1,26 @@
 return {
   {
     "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
     ft = "python",
     opts = {
       adapters = {
-        require("neotest-python")({
+        ["neotest-python"] = {
           runner = "pytest",
-          --python = "./.venv313/bin/python",
-          is_test_file = function(file_path)
-            local M = require("neotest-python.base")
-            local lib = require("neotest.lib")
-
-            local elems = vim.split(file_path, lib.files.sep)
-            return M.is_test_file(file_path) and vim.list_contains(elems, "test")
-          end,
-        }),
+          -- python = "./.venv/bin/python",
+          pytest_discover_instances = true,
+          -- is_test_file = function(file_path)
+          --   local M = require("neotest-python.base")
+          --   local lib = require("neotest.lib")
+          --
+          --   local elems = vim.split(file_path, lib.files.sep)
+          --   return vim.startswith(file_path, "test")
+          --   -- return M.is_test_file(file_path) and vim.list_contains(elems, "test")
+          -- end,
+        },
       },
     },
   },

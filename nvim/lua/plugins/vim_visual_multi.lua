9b6b8f7c4e00246dcd09dd84189f36af10bc428d
Max Chesterfield <chestm007@hotmail.com>
2026-09-08 15:27:08 +1000

    use multicursors instead of vim-visual-multi

--- nvim/lua/plugins/vim_visual_multi.lua
+++ nvim/lua/plugins/vim_visual_multi.lua
@@ -1,7 +1,11 @@
+-- NOTE: disabled as it is replaced by multicursors.
 return {
   {
-    "mg979/vim-visual-multi",
-    branch = "master",
+    -- "mg979/vim-visual-multi",
+    -- branch = "master",
+    "eyalk11/vim-visual-multi",
+    commit = "9efaa56",
+    enabled = false,
     init = function()
       vim.g.VM_maps = {
         ["Find Under"] = "<A-j>",
@@ -16,4 +20,59 @@ return {
       vim.g.VM_leader = "\\"
     end,
   },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = function(_, opts)
+  --     local cmp = require("blink.cmp")
+  --     vim.api.nvim_get_keymap("i")
+  --     vim.api.nvim_create_autocmd("User", {
+  --       pattern = "visual_multi_exit",
+  --       callback = function()
+  --         vim.keymap.set("i", "<CR>", function()
+  --           if cmp.is_visible() then
+  --             cmp.select_and_accept()
+  --             return ""
+  --           else
+  --             return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+  --           end
+  --         end, { expr = true, silent = true })
+  --
+  --         vim.keymap.set("i", "<Up>", function()
+  --           if cmp.is_visible() then
+  --             cmp.select_prev()
+  --             return ""
+  --           else
+  --             return "<Up>"
+  --             -- return vim.api.nvim_replace_termcodes("<Up>", true, false, true)
+  --           end
+  --         end)
+  --
+  --         vim.keymap.set("i", "<Down>", function()
+  --           if cmp.is_visible() then
+  --             cmp.select_next()
+  --             return ""
+  --           else
+  --             return "<Down>"
+  --             -- return vim.api.nvim_replace_termcodes("<Down>", true, false, true)
+  --           end
+  --         end)
+  --
+  --         -- if not opts.keymap["<Tab>"] then
+  --         --   if opts.keymap.preset == "super-tab" then -- super-tab
+  --         --     opts.keymap["<Tab>"] = {
+  --         --       require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+  --         --       LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
+  --         --       "fallback",
+  --         --     }
+  --         --   else -- other presets
+  --         --     opts.keymap["<Tab>"] = {
+  --         --       LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
+  --         --       "fallback",
+  --         --     }
+  --         --   end
+  --         -- end
+  --       end,
+  --     })
+  --   end,
+  -- },
 }

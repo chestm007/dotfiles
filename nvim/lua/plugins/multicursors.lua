6f5424f4f5a8d0270ec2b8dcdc3f75563b44c9cf
Max Chesterfield <chestm007@hotmail.com>
2026-09-14 15:39:09 +1000

    fix multicursor

--- nvim/lua/plugins/multicursors.lua
+++ nvim/lua/plugins/multicursors.lua
@@ -1,16 +1,21 @@
 return {
   {
     "smoka7/multicursors.nvim",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
     dependencies = {
       "nvimtools/hydra.nvim",
     },
-    opts = {},
+    opts = function(_, opts)
+      table.insert(opts, {
+        DEBUG_MODE = true,
+        create_commands = true,
+      })
+    end,
     cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
     keys = {
       {
-        mode = { "v", "n" },
-        "<A-j>",
+        -- mode = { "v", "n" },
+        "<leader>vm",
         "<cmd>MCstart<cr>",
         desc = "Create a selection for selected text or word under the cursor",
       },

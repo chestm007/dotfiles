dc1a91347a9f7a55e7a20bb862c6fcf02c922ccd
chestm007 <chestm007@hotmail.com>
2026-09-14 14:19:57 +1000

    overseer

---
+++ nvim/lua/plugins/overseer.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    lazy = false, -- plugin is self-lazy-loading
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "OverseerRun",
+      "OverseerTaskAction",
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>pw", "<cmd>OverseerToggle!<cr>",    desc = "Task list" },
+      { "<leader>po", "<cmd>OverseerRun<cr>",        desc = "Run task" },
+      { "<leader>pt", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>p", group = "overseer" },
+      },
+    },
+  },
+}
